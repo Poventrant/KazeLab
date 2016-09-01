e1a17b7a0c9948803850f042b19ac63d88162510
kaze <296306654@qq.com>
2016-09-01 10:33:17 +0800

    一个全是数字的大数组，除了其中一个数字出现2次外，其余的数字都出现了3次

--- src/kaze/java8/InvoiceTest.java
+++ src/kaze/java8/InvoiceTest.java
@@ -4,4 +4,6 @@ package kaze.java8;
  * Created by kaze on 2016/8/31.
  */
 public class InvoiceTest {
+    public static void main(String[] args) {
+    }
 }
--- src/kaze/other/NUMBER_2_IN_3.java
+++ src/kaze/other/NUMBER_2_IN_3.java
@@ -4,4 +4,29 @@ package kaze.other;
  * Created by kaze on 2016/9/1.
  */
 public class NUMBER_2_IN_3 {
+
+    static int get3IndexNumber(int n, int index) {
+        int result = 0;
+        for (int i = 0; i <= index; i++) {
+            result = n % 3;
+            n /= 3;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arrs = {1,1,1,2,3,3,3,4,4,2,4,9,9,9};
+        int ans = 0, pos = 0;
+        for (int k = 0; k < Integer.BYTES; k++) {
+            int sum = 0;
+            for (int i = 0; i < arrs.length; i++) {
+                sum += get3IndexNumber(arrs[i], pos);
+            }
+            sum = (2 * sum) % 3;
+            ans += sum * Math.pow(3, pos);
+            ++pos;
+        }
+        System.out.println(ans);
+    }
 }
