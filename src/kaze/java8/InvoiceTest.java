271c86031de9326e6d9f8b9e956ed87475362894
kaze <296306654@qq.com>
2016-09-01 10:32:33 +0800

    一个全是数字的大数组，除了其中一个数字出现2次外，其余的数字都出现了3次

---
+++ src/kaze/java8/InvoiceTest.java
@@ -0,0 +1,7 @@
+package kaze.java8;
+
+/**
+ * Created by kaze on 2016/8/31.
+ */
+public class InvoiceTest {
+}
---
+++ src/kaze/other/NUMBER_2_IN_3.java
@@ -0,0 +1,7 @@
+package kaze.other;
+
+/**
+ * Created by kaze on 2016/9/1.
+ */
+public class NUMBER_2_IN_3 {
+}
